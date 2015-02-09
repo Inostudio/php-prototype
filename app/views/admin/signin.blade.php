eb4f233e9477a5e940208159cdf331da66fbeab8
Vasiliy Gryaznoy <vasja930403@yandex.ru>
2015-02-09 22:51:32 +0300

    Style refactoring for admin

--- app/views/admin/signin.blade.php
+++ app/views/admin/signin.blade.php
@@ -7,6 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="icon" href="../../favicon.ico">
+        <link href="/admin/css/adminSignInStyle.css" rel="stylesheet">
 
         <title>Signin</title>
 
@@ -17,49 +18,6 @@
         
 
         <!-- Custom styles for this template -->
-        <style type="text/css">
-            body {
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: #eee;
-            }
-
-            .form-signin {
-                max-width: 330px;
-                padding: 15px;
-                margin: 0 auto;
-            }
-            .form-signin .form-signin-heading,
-            .form-signin .checkbox {
-                margin-bottom: 10px;
-            }
-            .form-signin .checkbox {
-                font-weight: normal;
-            }
-            .form-signin .form-control {
-                position: relative;
-                height: auto;
-                -webkit-box-sizing: border-box;
-                -moz-box-sizing: border-box;
-                box-sizing: border-box;
-                padding: 10px;
-                font-size: 16px;
-            }
-            .form-signin .form-control:focus {
-                z-index: 2;
-            }
-            .form-signin input[type="email"] {
-                margin-bottom: -1px;
-                border-bottom-right-radius: 0;
-                border-bottom-left-radius: 0;
-            }
-            .form-signin input[type="password"] {
-                margin-bottom: 10px;
-                border-top-left-radius: 0;
-                border-top-right-radius: 0;
-            }        
-
-        </style>
 
         <script src="/vendor/angular/angular.min.js"></script>
         <script src="/vendor/ui-bootstrap/ui-bootstrap-tpls-0.11.2.js"></script>
