97e5390135cf246cdf2a2d86600de31e0c707965
Vasiliy Gryaznoy <vasja930403@yandex.ru>
2015-02-11 20:11:14 +0300

    Change title personalization button for admin

--- app/views/admin/angular/adminPartials/personalization.blade.php
+++ app/views/admin/angular/adminPartials/personalization.blade.php
@@ -2,36 +2,36 @@
 <h3 class="sub-header"><%trans('admin/personalization.change_colors')%></h3>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.activeClass" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.activeClass" ng-disabled="!vm.cookieEnable">.active</button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.activeClass" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.active')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultActive()"><%trans('admin/personalization.default')%></button>
 </div>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.btnPrimary" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.btnPrimary" ng-disabled="!vm.cookieEnable">.btn-primary</button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.btnPrimary" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.button')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultBtnPrimary()"><%trans('admin/personalization.default')%></button>
 </div>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.navbarInverse" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.navbarInverse" ng-disabled="!vm.cookieEnable">.navbar-inverse</button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.navbarInverse" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.navbar')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultNavbarInverse()"><%trans('admin/personalization.default')%></button>
 </div>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.sidebar" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.sidebar" ng-disabled="!vm.cookieEnable">.sidebar</button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.sidebar" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.sidebar')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultSidebar()"><%trans('admin/personalization.default')%></button>
 </div>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.a" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.a" ng-disabled="!vm.cookieEnable"><.a></button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.a" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.links')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultA()"><%trans('admin/personalization.default')%></button>
 </div>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.pageHeader" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.pageHeader" ng-disabled="!vm.cookieEnable">.page-header</button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.pageHeader" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.pageName')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultPageHeader()"><%trans('admin/personalization.default')%></button>
 </div>
 <div class="form-inline resonalizationMargin">
     <input type="text" ng-model="vm.colors.subHeader" class="form-control" ng-disabled="!vm.cookieEnable"/>
-    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.subHeader" ng-disabled="!vm.cookieEnable">.sub-header</button>
+    <button class="btn btn-primary" colorpicker type="button" colorpicker-position="top" ng-model="vm.colors.subHeader" ng-disabled="!vm.cookieEnable"><%trans('admin/personalization.pageName2')%></button>
     <button class="btn btn-primary" ng-disabled="!vm.cookieEnable" ng-click="vm.defaultSubHeader()"><%trans('admin/personalization.default')%></button>
 </div>
